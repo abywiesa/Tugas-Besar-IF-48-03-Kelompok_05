fb8ef195fbef7c597eed42682ab2e664a01a472a
abywiesa <adsanu2@gmail.com>
2025-11-27 20:09:45 +0700

    Update code dari Adam

--- Tugas-Besar-IF-48-03-Kelompok_05/Jurnalis.h
+++ Tugas-Besar-IF-48-03-Kelompok_05/Jurnalis.h
@@ -1,6 +1,43 @@
-#ifndef JURNALIS_H_INCLUDED
-#define JURNALIS_H_INCLUDED
+#ifndef JURNALIS_H
+#define JURNALIS_H
 
+#include <iostream>
+#include <string>
+using namespace std;
 
+struct Jurnalis {
+    string id_jurnalis;
+    string nama;
+    string email;
+    string bidang;
+    int tahun_bergabung;
+};
 
-#endif // JURNALIS_H_INCLUDED
+typedef struct elemenJurnalis *addressJurnalis;
+
+struct elemenJurnalis {
+    Jurnalis info;
+    addressJurnalis next;
+};
+
+struct ListJurnalis {
+    addressJurnalis first;
+};
+
+void buatListJurnalis(ListJurnalis &L);
+addressJurnalis alokasiJurnalis(Jurnalis data);
+void dealokasiJurnalis(addressJurnalis P);
+void sisipJurnalisAwalBerdasarkanTahun(ListJurnalis &L, addressJurnalis P);
+void sisipJurnalisAkhirBerdasarkanBidang(ListJurnalis &L, addressJurnalis P);
+void sisipJurnalisSetelahBerdasarkanEmail(ListJurnalis &L, addressJurnalis Prec, addressJurnalis P);
+void hapusJurnalisAwalBerdasarkanTahun(ListJurnalis &L, addressJurnalis &P);
+void hapusJurnalisAkhirBerdasarkanBidang(ListJurnalis &L, addressJurnalis &P);
+void hapusJurnalisSetelahBerdasarkanNama(ListJurnalis &L, addressJurnalis Prec, addressJurnalis &P);
+addressJurnalis cariJurnalisByID(ListJurnalis L, string id);
+addressJurnalis cariJurnalisByNama(ListJurnalis L, string nama);
+int hitungTotalJurnalis(ListJurnalis L);
+float hitungRataRataTahunBergabung(ListJurnalis L);
+void tampilkanSemuaJurnalis(ListJurnalis L);
+void tampilkanJurnalisPalingSenior(ListJurnalis L);
+
+#endif
