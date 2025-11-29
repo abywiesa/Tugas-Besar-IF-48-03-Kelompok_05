0087a6a6310de09a649bbae91b13a806d62f7d4a
Rifat <rifatwicaksono17@gmail.com>
2025-11-29 16:36:17 +0700

    Menambahkan Berita dan jurnalis

--- Tugas-Besar-IF-48-03-Kelompok_05/Berita.h
+++ Tugas-Besar-IF-48-03-Kelompok_05/Berita.h
@@ -1,13 +1,41 @@
-#ifndef BERITA_H
-#define BERITA_H
+#ifndef BERITA_H_INCLUDED
+#define BERITA_H_INCLUDED
 
 #include <iostream>
-#include <string>
 using namespace std;
 
-struct Berita {
-    string id_berita;
+struct dataBerita {
+    string id;
     string judul;
     string kategori;
-    string tanggal_terbit;
-    int jumlah_kata;
+    string tanggal;
+};
+
+struct elemenBerita {
+    dataBerita info;
+    elemenBerita *next;
+    elemenBerita *prev;
+};
+
+struct listBerita {
+    elemenBerita *first;
+    elemenBerita *last;
+};
+
+void buatListBerita(listBerita &L);
+elemenBerita* buatElemenBerita(dataBerita dataBaru);
+bool listBeritaKosong(listBerita L);
+void tampilkanSemuaBerita(listBerita L);
+void sisipBeritaAwal(listBerita &L, elemenBerita *p);
+void sisipBeritaAkhir(listBerita &L, elemenBerita *p);
+void sisipBeritaSetelah(listBerita &L, string idPatokan, elemenBerita *p);
+elemenBerita* cariBerita(listBerita L, string idDicari);
+void hapusBeritaAwal(listBerita &L);
+void hapusBeritaAkhir(listBerita &L);
+void hapusBeritaTertentu(listBerita &L, string idHapus);
+int hitungJumlahBerita(listBerita L);
+elemenBerita* cariBeritaKategori(listBerita L, string kategori);
+void updateDataBerita(listBerita &L, string idTarget);
+void menuKelolaBerita(listBerita &L);
+
+#endif
--- Tugas-Besar-IF-48-03-Kelompok_05/Jurnalis.h
+++ Tugas-Besar-IF-48-03-Kelompok_05/Jurnalis.h
@@ -1,27 +1,39 @@
-#ifndef JURNALIS_H
-#define JURNALIS_H
+#ifndef JURNALIS_H_INCLUDED
+#define JURNALIS_H_INCLUDED
 
 #include <iostream>
-#include <string>
 using namespace std;
 
-struct Jurnalis {
-    string id_jurnalis;
+struct dataJurnalis {
+    string id;
     string nama;
-    string email;
-    string bidang;
-    int tahun_bergabung;
+    int umur;
+    string gender;
 };
-
-typedef struct elemenJurnalis *addressJurnalis;
 
 struct elemenJurnalis {
-    Jurnalis info;
-    addressJurnalis next;
+    dataJurnalis info;
+    elemenJurnalis *next;
 };
 
-struct ListJurnalis {
-    addressJurnalis first;
+struct listJurnalis {
+    elemenJurnalis *first;
 };
+
+void buatListJurnalis(listJurnalis &L);
+elemenJurnalis* buatElemenJurnalis(dataJurnalis dataBaru);
+bool listJurnalisKosong(listJurnalis L);
+void tampilkanSemuaJurnalis(listJurnalis L);
+void sisipJurnalisAwal(listJurnalis &L, elemenJurnalis *p);
+void sisipJurnalisAkhir(listJurnalis &L, elemenJurnalis *p);
+void sisipJurnalisSetelah(listJurnalis &L, string idPatokan, elemenJurnalis *p);
+elemenJurnalis* cariJurnalis(listJurnalis L, string idDicari);
+void hapusJurnalisAwal(listJurnalis &L);
+void hapusJurnalisAkhir(listJurnalis &L);
+void hapusJurnalisTertentu(listJurnalis &L, string idHapus);
+int hitungJumlahJurnalis(listJurnalis L);
+elemenJurnalis* cariJurnalisNama(listJurnalis L, string nama);
+void updateDataJurnalis(listJurnalis &L, string idTarget);
+void menuKelolaJurnalis(listJurnalis &L);
 
 #endif
