565eef2e7cdc8eef66c35b805ac32851eb8c5ab2
abywiesa <adsanu2@gmail.com>
2025-12-05 15:10:45 +0700

    menambahkan primitif

--- Tugas-Besar-IF-48-03-Kelompok_05/Berita.h
+++ Tugas-Besar-IF-48-03-Kelompok_05/Berita.h
@@ -1,13 +1,46 @@
-#ifndef BERITA_H
-#define BERITA_H
+#ifndef BERITA_H_INCLUDED
+#define BERITA_H_INCLUDED
 
 #include <iostream>
-#include <string>
 using namespace std;
 
-struct Berita {
-    string id_berita;
+typedef struct elemenBerita *adrBerita;
+
+struct dataBerita {
+    string id;
     string judul;
     string kategori;
-    string tanggal_terbit;
-    int jumlah_kata;
+    string tanggal;
+};
+
+struct elemenBerita {
+    dataBerita info;
+    adrBerita next;
+    adrBerita prev;
+};
+
+struct listBerita {
+    adrBerita first;
+    adrBerita last;
+};
+
+void createNewsList(listBerita &L);
+adrBerita createNewsElement(dataBerita newData);
+bool isNewsListEmpty(listBerita L);
+void showAllNews(listBerita L);
+void insertNewsFirst(listBerita &L, adrBerita p);
+void insertNewsLast(listBerita &L, adrBerita p);
+void insertNewsAfter(listBerita &L, string idKey, adrBerita p);
+adrBerita findNewsByID(listBerita L, string idKey);
+adrBerita findNewsByCategory(listBerita L, string categoryKey);
+void deleteNewsFirst(listBerita &L);
+void deleteNewsLast(listBerita &L);
+void deleteNewsByID(listBerita &L, string idDelete);
+void updateNews(listBerita &L, string idTarget);
+int countNews(listBerita L);
+int countNewsByCategory(listBerita L, string categoryKey);
+adrBerita getNewestNews(listBerita L);
+adrBerita getOldestNews(listBerita L);
+void menuNewsAdmin(listBerita &L);
+
+#endif
--- Tugas-Besar-IF-48-03-Kelompok_05/Jurnalis.h
+++ Tugas-Besar-IF-48-03-Kelompok_05/Jurnalis.h
@@ -1,27 +1,43 @@
-#ifndef JURNALIS_H
-#define JURNALIS_H
+#ifndef JURNALIS_H_INCLUDED
+#define JURNALIS_H_INCLUDED
 
 #include <iostream>
-#include <string>
 using namespace std;
 
-struct Jurnalis {
-    string id_jurnalis;
-    string nama;
-    string email;
-    string bidang;
-    int tahun_bergabung;
-};
+typedef struct elemenJurnalis *adrJurnalis;
 
-typedef struct elemenJurnalis *addressJurnalis;
+struct dataJurnalis {
+    string id;
+    string nama;
+    int umur;
+    string gender;
+};
 
 struct elemenJurnalis {
-    Jurnalis info;
-    addressJurnalis next;
+    dataJurnalis info;
+    adrJurnalis next;
 };
 
-struct ListJurnalis {
-    addressJurnalis first;
+struct listJurnalis {
+    adrJurnalis first;
 };
+
+void createJournalistList(listJurnalis &L);
+adrJurnalis createJournalistElement(dataJurnalis newData);
+bool isJournalistListEmpty(listJurnalis L);
+void showAllJournalists(listJurnalis L);
+void insertJournalistFirst(listJurnalis &L, adrJurnalis p);
+void insertJournalistLast(listJurnalis &L, adrJurnalis p);
+void insertJournalistAfter(listJurnalis &L, string idKey, adrJurnalis p);
+adrJurnalis findJournalistByID(listJurnalis L, string idKey);
+adrJurnalis findJournalistByName(listJurnalis L, string nameKey);
+void deleteJournalistFirst(listJurnalis &L);
+void deleteJournalistLast(listJurnalis &L);
+void deleteJournalistByID(listJurnalis &L, string idDelete);
+void updateJournalist(listJurnalis &L, string idTarget);
+int countJournalists(listJurnalis L);
+void showMaleJournalists(listJurnalis L);
+void showFemaleJournalists(listJurnalis L);
+void menuJournalistAdmin(listJurnalis &L);
 
 #endif
